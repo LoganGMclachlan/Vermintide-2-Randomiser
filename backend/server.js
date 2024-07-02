a1bddb172b074f65785632c9d4b1d074bd53fb19
Logan Mclachlan <logan.g.mclachlan@gmail.com>
2024-07-02 19:42:18 +0100

    added routes to query db for required data

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,43 @@ const db = sql.createConnection({
 })
 
 app.listen(8080, () => {
-    console.log("Listening on part 8080.")
+    console.log("Listening on port 8080.\nhttp://localhost:8080")
+})
+
+// defualt route return all career data
+app.get('/', (req, res) => {
+    db.query(`SELECT * FROM Careers`, (err,data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+// finds and returns specific career data from title
+app.get('/getCareers', (req, res) => {
+    db.query(`SELECT * FROM Careers WHERE id=${req.body.id}`, (err,data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+// finds and returns all weapon data for a given career
+app.get('/getWeapons', (req, res) => {
+    // query includes 2 joins to connect 3 tables
+    const query = `SELECT weapons.weapon_name, weapons.weapon_type 
+        FROM weapons, career_weapons, careers 
+        WHERE careers.id = career_weapons.career_id & 
+        career_weapons.weapon_id = weapons.id &
+        careers.id = ${req.body.id}`
+    db.query(query, (err,data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
+})
+
+// finds and returns all talent data for specified career
+app.get('/getTalents', (req, res) => {
+    db.query(`SELECT * FROM Talents WHERE career_id='${req.body.id}'`, (err,data) => {
+        if (err) return res.json(err)
+        return res.json(data)
+    })
 })
